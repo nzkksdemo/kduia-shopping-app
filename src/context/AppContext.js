5d2ccad26fa63477715396f2f63d4d5bba9aa0d6
nzkksdemo <nzkksdemo@gmail.com>
2024-03-06 16:21:08 -0500

    AppContext: Add the base structure

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,32 @@
 import React, { createContext, useReducer } from 'react';
+
+export const AppReducer = (state, action) => {
+    switch (action.type) {
+        case value:
+            
+            return {...state};
+    
+        default:
+            return {...state};
+    }
+}
+
+const initialState = {
+
+};
+
+export const AppContext = () => createContext();
+
+export const AppProvider = ({children}) => {
+    const {state, dispatch} = useReducer(AppReducer, initialState);
+
+    return (
+        <AppContext.Provider 
+            value={{
+                dispatch
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
+};
